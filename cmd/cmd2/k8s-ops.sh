c06a31ff8e6a2f454028fd2a56ec8ec548570087
Oluwaseun Ogunjimi <seun.ogunjimi@gmail.com>
2025-05-01 08:36:48 +0200

    init service apps

--- cmd/cmd2/k8s-ops.sh
+++ cmd/cmd2/k8s-ops.sh
@@ -212,6 +212,7 @@ install_argocd() {
     # Patch service to use NodePort
     kubectl patch svc argocd-server -n $ARGOCD_NAMESPACE -p '{"spec": {"type": "NodePort", "ports": [{"nodePort": '"$ARGOCD_PORT"', "port": 80, "protocol": "TCP", "targetPort": 8080}]}}'
 
+#kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort", "ports": [{"nodePort": "30080", "port": 80, "protocol": "TCP", "targetPort": 8080}]}}'
 
     # Get initial admin password
     ARGOCD_PASSWORD=$(kubectl -n $ARGOCD_NAMESPACE get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
